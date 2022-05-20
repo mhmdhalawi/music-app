de016b1856345e407cc6043f946471d0981f6211
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-05-20 17:54:33 +0300

    remove formkit for veevalidate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-const formKitTailwind = require('@formkit/themes/tailwindcss');
-
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@formkit/themes/tailwindcss')],
+  plugins: [],
 };
